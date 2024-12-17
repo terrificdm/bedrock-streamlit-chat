f11f03c8547b78fa91aecb59b4efce605f3889c8
Guo <guodaomi@08f8bc6ab080.ant.amazon.com>
2024-12-17 10:28:10 +0800

    add Amazon Nova series of models

--- bedrock_streamlit_converseAPI.py
+++ bedrock_streamlit_converseAPI.py
@@ -88,10 +88,10 @@ def main():
         with col2:
             st.title("Bedrock-Claude-Chat")
 
-        model_id = st.selectbox('Choose a Model', ('Amazon Nova Pro', 'Amazon Nova Lite', 'Anthropic Claude-V3-Haiku', 'Anthropic Claude-V3-Sonnet', 'Anthropic Claude-V3.5-Sonnet', 'Anthropic Claude-V3.5-Sonnet-v2'), index=4, label_visibility="collapsed")
+        model_id = st.selectbox('Choose a Model', ('Amazon Nova Lite', 'Amazon Nova Pro', 'Anthropic Claude-V3-Haiku', 'Anthropic Claude-V3-Sonnet', 'Anthropic Claude-V3.5-Sonnet', 'Anthropic Claude-V3.5-Sonnet-v2'), index=5, label_visibility="collapsed")
         model_id = {
-            'Amazon Nova Pro': 'amazon.nova-pro-v1:0',
             'Amazon Nova Lite': 'amazon.nova-lite-v1:0',
+            'Amazon Nova Pro': 'amazon.nova-pro-v1:0',
             'Anthropic Claude-V3-Haiku': 'anthropic.claude-3-haiku-20240307-v1:0',
             'Anthropic Claude-V3-Sonnet': 'anthropic.claude-3-sonnet-20240229-v1:0',
             'Anthropic Claude-V3.5-Sonnet': 'anthropic.claude-3-5-sonnet-20240620-v1:0',
